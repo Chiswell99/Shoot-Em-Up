b93a567c3ee2895782a6aeff09953dea6b4ed78d
Sidkiwi <dssalazar86@gmail.com>
2022-10-13 17:32:24 -0300

    Prueba Input

--- Shmup/Assets/Scripts/Input/InputKeyboardListener.cs
+++ Shmup/Assets/Scripts/Input/InputKeyboardListener.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class InputKeyboardListener : MonoBehaviour, IInputeable
 {
-    
+    float butX;
+    float butY;
+    public Vector3 direction;
     public void GetDirection(Vector3 direction)
     {
         InputProvider.TriggerDirection(direction);
@@ -21,17 +23,33 @@ public class InputKeyboardListener : MonoBehaviour, IInputeable
         }
 
 
-        GetDirection(new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
+        //GetDirection(new Vector3(butX, butY));
+        GetDirection(direction);
+        //GetDirection(new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
 
 
     }
 
-    //public void MoveButtonX()
+    public void MovementX(float valX)
+    {
+        direction = new Vector3(valX, 0, 0);
+    }
+
+    public void MovementY(float valY)
+    {
+        direction = new Vector3(0, valY, 0);
+    }
+    //public void MoveButtonX(float val)
     //{
-    //    newDirection = new Vector3(1, 0, 0);
-    //    Debug.Log("Entro al X");
+    //    butX = val;
+    //    //butX = Mathf.Clamp(butX, -1, 1);
     //}
 
+    //public void MoveButtonY(float val)
+    //{
+    //    butY = val;
+    //    //butY = Mathf.Clamp(butY, -1, 1);
+    //}
     //public void MoveButtonY()
     //{
     //    newDirection = new Vector3(0, 1, 0);
